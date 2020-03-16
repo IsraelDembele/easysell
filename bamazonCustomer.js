2cf8904270af7ff1dbcfebd0a5d23a2325df9355
israel Dembele <demisrael00@gmail.com>
2020-03-16 13:10:19 -0400

    corrections

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,7 +4,7 @@ var Table = require ('cli-table');
 
 var connection = mysql.createConnection({
     host : "localhost",
-    port : 
+    port : 3306, 
     user: "root",
     password :"password", 
     database : "bamazon_db"
